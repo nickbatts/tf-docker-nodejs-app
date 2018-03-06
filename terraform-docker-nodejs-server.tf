56c2b1fbaeea2a4236aa56968cb6e19dd345b404
Nick Batts <nickbatts@users.noreply.github.com>
2018-03-05 22:37:50 -0700

    added second nodejs container and nginx round robin load balancing

--- terraform-docker-nodejs-server.tf
+++ terraform-docker-nodejs-server.tf
@@ -2,6 +2,10 @@
 provider "docker" {}
 
 # terraform can only pull from public Docker Hub repositories
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
 resource "docker_image" "node" {
   name = "node:alpine"
 }
@@ -10,11 +14,12 @@ resource "docker_image" "node" {
 resource "docker_container" "webapp" {
   image = "${docker_image.node.name}"
   name  = "webapp"
+  env = ["NODE_PORT=3000"]
   #command = ["npm","install","--save"]
   user = "node"
   entrypoint = ["nohup","node","app/index.js","&"]
   must_run = true
-  restart = "unless-stopped"
+  restart = "always"
   publish_all_ports = true
 
   ports {
@@ -39,21 +44,71 @@ resource "docker_container" "webapp" {
   }
 }
 
-
-
-# Create a container
+# Create container #2
 resource "docker_container" "webapp2" {
-  image = "${docker_image.node.name}"
-  name  = "webapp2"
-  entrypoint = ["nohup","node","app/index.js","&"]
-  links = ["webapp:webapp"]
-  restart = "unless-stopped"
-  publish_all_ports = true
-  depends_on = ["docker_container.webapp"]
+  image          = "${docker_image.node.name}"
+  name           = "webapp2"
+  env            = ["NODE_PORT=3001"]
+  #command       = ["npm","install","--save"]
+  user           = "node"
+  entrypoint     = ["nohup","node","app/index.js","&"]
+  must_run       = true
+  restart        = "always"
+  publish_all_ports   = true
 
+  ports {
+    internal     = 3001
+    protocol     = "tcp"
+  }
 
   upload {
-  	content = "${file("index.js")}"
-  	file = "app/index.js"
+    content      = "${file("package.json")}"
+    file         = "app/package.json"
+  }
+
+  upload {
+    content      = "${file("index.js")}"
+    file         = "app/index.js"
+  }
+
+  labels {
+    Description = "terraform-managed docker service for a nodejs app"
+    Version     = "0.5.0"
+    Author      = "Nick Batts"
+  }
+}
+
+
+# Create NGINX container
+resource "docker_container" "nginx" {
+  image             = "${docker_image.nginx.name}"
+  name              = "nginx"
+  #command          = ["rm -rf etc/nginx/conf.d/default.conf"]
+  #entrypoint       = ["/usr/sbin/nginx -g"]
+  links             = ["webapp:webapp","webapp2:webapp2"]
+  must_run          = true
+  restart           = "always"
+  publish_all_ports = false
+  depends_on        = ["docker_container.webapp"]
+
+  ports {
+    internal        = 8080
+    external        = 8888
+    protocol        = "tcp"
+  }
+
+  upload {
+  	content = "${file("nginx.conf")}"
+  	file = "/etc/nginx/nginx.conf"
   }
  }
+
+
+
+output "nginx_ip" {
+  value = "${docker_container.nginx.ip_address}"
+}
+
+output "node_1_ip" {
+  value = "${docker_container.webapp.ip_address}"
+}
